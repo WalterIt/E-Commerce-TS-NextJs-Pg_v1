6c56d78f359e59ddadbd327dead26084e925ac3c
WalterIT <wshiruba@gmail.com>
2024-07-08 08:54:01 -0400

    PREPARED APP To Connect to Vercel Database

--- db/drizzle.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle} from 'drizzle-orm/vercel-postgres';
-import postgres from 'vercel/postgres';
-import * as schema from '@/db/schema';
-// config({ path: ".env" })
-
-const client = postgres(process.env.AUTH_DRIZZLE_URL!, {prepare: false});
-export const db = drizzle(client, { schema });
